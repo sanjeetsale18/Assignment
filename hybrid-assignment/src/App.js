7be920b1f60b117b417fafb14b50da32c809954e
sanjeetsale <salesanjeet8@gmail.com>
2022-05-21 16:10:41 +0530

    First commit
    Installed react app  and created component.

---
+++ hybrid-assignment/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./Component/Home";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
